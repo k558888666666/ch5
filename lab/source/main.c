070ba11346decd3b2b0c4e6643a68994cb943af1
jojo <k558888666666@gmail.com>
2018-11-23 17:42:37 +0800

    first

---
+++ lab/source/main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define STUDENTS 3
+#define EXAMS 4
+
+void minimum(int grade[][EXAMS], int pupils, int tests)
+{
+	int lowGrade = 100;
+	for (int i = 0; i <= pupils - 1; i++)
+	{
+		for (int j = 0; j <= tests - 1; j++)
+		{
+			if (grade[i][j]<lowGrade)
+			{
+				lowGrade = grade[i][j];
+			}
+		}
+	}
+	printf("\n\tThe lowest grade is %d\n", lowGrade);
+}
+void maximum(int grade[][EXAMS], int pupils, int tests)
+{
+	int highGrade = 0;
+	for (int i = 0; i <= pupils - 1; i++)
+	{
+		for (int j = 0; j <= tests - 1; j++)
+		{
+			if (grade[i][j] > highGrade)
+			{
+				highGrade = grade[i][j];
+			}
+		}
+	}
+	printf("\n\tThe highest grade is %d\n", highGrade);
+}
+void average(int grade[][EXAMS], int pupils, int tests)
+{
+	int total;
+	int i;
+	for ( i = 0; i <= pupils - 1; i++)
+	{
+		total = 0;
+		for (int j = 0; j <= tests - 1; j++)
+		{
+			total += grade[i][j];
+		}
+		printf("\n\tThe average for student %d is %.1f\n", i + 1, (double)total / tests);
+	}
+	
+}
+void printArray(int grade[][EXAMS], int pupils, int tests)
+{
+	int i, j;
+	printf("\n\t                  [0]   [1]    [2]    [3]");
+	for ( i = 0; i <= pupils - 1; i++)
+	{
+		printf("\n\tstudentGrades[%d]  ", i);
+		for ( j = 0; j <= tests -1; j++)
+		{
+			printf("%-7d", grade[i][j]);
+		}
+	}
+	printf("\n");
+}
+void printMenu(void)
+{
+	printf("\n\tEnter a choice:\n"
+		"\t  0  Print the array of grades\n"
+		"\t  1  Find the minimum grade\n"
+		"\t  2  Find the maximum grade\n"
+		"\t  3  Print the average on all"
+		" tests for each student\n"
+		"\t  4  End program\n""\t?");
+}
+
+
+int main()
+{
+	void(*processGrades[4])(int[][EXAMS], int, int)
+		= { printArray, minimum, maximum, average };
+	int choice = 0;
+	int studentGrades[STUDENTS][EXAMS] = { {77,68,86,73},{96,87,89,78},{70,90,86,81} };
+	
+	while (choice != 4)
+	{
+		do
+		{
+			printMenu();
+			scanf_s("%d", &choice);
+		} while (choice<0 || choice>4);
+		if (choice != 4)
+		{
+			(*processGrades[choice])(studentGrades, STUDENTS, EXAMS);
+		}
+		else
+		{
+			printf("Program Ended.\n");
+		}
+	}
+	system("pause");
+	return 0;
+}
